b12959d37edebb1718e7f93c712a6175ff52132d
Omer Dagry <93447258+Omer-Dagry@users.noreply.github.com>
2024-06-03 00:58:39 +0300

    TurboAPI - example

---
+++ app.py
@@ -0,0 +1,29 @@
+from pathlib import Path
+
+from TurboAPI import TurboAPI, Request, Response
+from TurboAPI.http import ContentTypes
+from TurboAPI.simple_server import SimpleServer
+
+app = TurboAPI()
+app.mount_folder("/webroot")
+
+
+@app.get("/")
+def index(response: Response) -> str:
+    response.CONTENT_TYPE = ContentTypes.HTML.value
+    return Path("webroot/index.html").read_text()
+
+
+@app.get("/test")
+def test(request: Request, response: Response) -> bytes:
+    print(request)
+    print(response)
+    return b"hello world!"
+
+
+def main() -> None:
+    SimpleServer("0.0.0.0", 80).run(app)
+
+
+if __name__ == '__main__':
+    main()
